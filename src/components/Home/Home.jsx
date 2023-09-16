22715c3d01f2a446c6ab9fb5a02b106be862b9f5
Mahmodul7314 <mahmudulhasan7314@gmail.com>
2023-09-16 03:36:05 -0700

    json file path changed

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -18,7 +18,7 @@ const Home = () => {
 
 
     useEffect(()=>{
-        fetch('public/data.json')
+        fetch('./data.json')
         .then((res) => res.json())
         .then((data) => setCourses(data));
     },[]);
